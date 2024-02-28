eb5be264e73a34b9280daf5185b1df5a549c97d0
Md. Abdul Hai <mdabdulhai2506@gmail.com>
2024-02-28 12:56:07 +0600

    Application Initialised

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Rubel());
+}
+
+class Rubel extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScrean(),
+    );
+  }
+}
+
+class HomeScrean extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.home),
+          title: Text('To Do App'),
+          backgroundColor: Colors.indigo,
+
+        ),
+        body: Center(child: Column
+        (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {
+              showDialog
+                (context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Message'),
+                      content: Text('This is a message content'),
+                      actions: [
+                        TextButton(onPressed: () {},
+                          child: Text('Cancel'),
+                          style: TextButton.styleFrom(
+                            backgroundColor: Colors.teal,
+                            foregroundColor: Colors.white,
+                          )
+                        )
+                      ],
+                    );
+                  }
+              );
+              
+            }, child: Text('Tap'),
+                style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.indigo,
+                  foregroundColor: Colors.amberAccent,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                  shadowColor: Colors.black87
+        )
+            ),
+
+            IconButton(onPressed: (){}, icon: Icon(Icons.add_a_photo),)
+          ],
+        ), ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+        floatingActionButton: FloatingActionButton (onPressed: ()
+    {
+      showAboutDialog(context: context);
+    },child: Icon(Icons.search),)
+
+    );
+  }
+}
