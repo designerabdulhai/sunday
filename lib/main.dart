391f20c5ef186e96433919a461141d1b4494d885
Md. Abdul Hai <mdabdulhai2506@gmail.com>
2024-02-28 16:57:34 +0600

    Button Child change

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class Rubel extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,51 +22,52 @@ class HomeScrean extends StatelessWidget {
           leading: Icon(Icons.home),
           title: Text('To Do App'),
           backgroundColor: Colors.indigo,
-
         ),
-        body: Center(child: Column
-        (
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: () {
-              showDialog
-                (context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Message'),
-                      content: Text('This is a message content'),
-                      actions: [
-                        TextButton(onPressed: () {},
-                          child: Text('Cancel'),
-                          style: TextButton.styleFrom(
-                            backgroundColor: Colors.teal,
-                            foregroundColor: Colors.white,
-                          )
-                        )
-                      ],
-                    );
-                  }
-              );
-              
-            }, child: Text('Tap'),
-                style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.indigo,
-                  foregroundColor: Colors.amberAccent,
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  shadowColor: Colors.black87
-        )
-            ),
-
-            IconButton(onPressed: (){}, icon: Icon(Icons.add_a_photo),)
-          ],
-        ), ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Message'),
+                            content: Text('This is a message content'),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text('Cancel'),
+                                  style: TextButton.styleFrom(
+                                    backgroundColor: Colors.teal,
+                                    foregroundColor: Colors.white,
+                                  ))
+                            ],
+                          );
+                        });
+                  },
+                  child: Text('Click Here'),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.indigo,
+                      foregroundColor: Colors.amberAccent,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                      shadowColor: Colors.black)),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add_a_photo),
+              )
+            ],
+          ),
+        ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
-        floatingActionButton: FloatingActionButton (onPressed: ()
-    {
-      showAboutDialog(context: context);
-    },child: Icon(Icons.search),)
-
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showAboutDialog(context: context);
+          },
+          child: Icon(Icons.search),
+        ));
   }
 }
